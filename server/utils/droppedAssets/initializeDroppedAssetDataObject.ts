c1af980d851bf3907bb3ef40d1cead058e1d103e
Lina <linabell@mail.com>
2025-05-12 12:50:45 -0700

    add catch to data object update

--- server/utils/droppedAssets/initializeDroppedAssetDataObject.ts
+++ server/utils/droppedAssets/initializeDroppedAssetDataObject.ts
@@ -6,7 +6,9 @@ export const initializeDroppedAssetDataObject = async (droppedAsset: IDroppedAss
     if (!droppedAsset?.dataObject?.droppedAssetCount) {
       // adding a lockId and releaseLock will prevent race conditions and ensure the data object is being updated only once until either the time has passed or the operation is complete
       const lockId = `${droppedAsset.id}-${new Date(Math.round(new Date().getTime() / 60000) * 60000)}`;
-      await droppedAsset.setDataObject({ droppedAssetCount: 0 }, { lock: { lockId, releaseLock: true } });
+      await droppedAsset
+        .setDataObject({ droppedAssetCount: 0 }, { lock: { lockId, releaseLock: true } })
+        .catch(() => console.warn("Unable to acquire lock, another process may be updating the data object"));
     }
 
     return;
